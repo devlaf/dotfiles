5afb108b8a96cf34e77d84616a7d486e9a4a1c5b
devlaf <devlaf@users.noreply.github.com>
2017-05-25 08:06:45 -0400

    Add tmux conf

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ dotfiles_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$HOME"
 ln -f -s "$dotfiles_dir/zsh/zshrc" ".zshrc"
+ln -f -s "$dotfiles_dir/tmux/tmux.conf" ".tmux.conf"
 
 cd "$HOME/.config"
 ln -fsn "$dotfiles_dir/nvim" "nvim"
+
+cd "$HOME/.config/i3"
+ln -fsn "$dotfiles_dir/i3/config" "config"
