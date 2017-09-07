6bfb999f60d975bf2325d202fa3f2b7c900cc027
devlaf <devlaf@users.noreply.github.com>
2017-09-07 07:36:45 -0400

    add ros lunar

--- zsh/ros/ros_setup.sh
+++ zsh/ros/ros_setup.sh
@@ -3,8 +3,16 @@
 # (note: Python 2.7 required for some older packages)
 #
 
+lunar() {
+  source /opt/ros/lunar/setup.zsh
+  export PYTHONPATH=/opt/ros/lunar/lib/python2.7/site-packages:$PYTHONPATH
+  export PKG_CONFIG_PATH="/opt/ros/lunar/lib/pkgconfig:$PKG_CONFIG_PATH"
+
+  alias catkin_make="catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python2 -DPYTHON_INCLUDE_DIR=/usr/include/python2.7 -DPYTHON_LIBRARY=/usr/lib/libpython2.7.so"
+}
+
 jade() {
-  source /opt/ros/jade/setup.bash
+  source /opt/ros/jade/setup.zsh
   export PYTHONPATH=/opt/ros/jade/lib/python2.7/site-packages:$PYTHONPATH
   export PKG_CONFIG_PATH="/opt/ros/jade/lib/pkgconfig:$PKG_CONFIG_PATH"
 
@@ -17,7 +25,7 @@ jade() {
 }
 
 indigo() {
-  source /opt/ros/indigo/setup.bash
+  source /opt/ros/indigo/setup.zsh
   export PYTHONPATH=/usr/lib/python2.7/site-packages:/opt/ros/indigo/lib/python2.7/site-packages:$PYTHONPATH
   export PKG_CONFIG_PATH="/opt/ros/indigo/lib/pkgconfig:$PKG_CONFIG_PATH"
 
