0abec05b95c6a18926636f010e68cf27733ff4a3
devlaf <devlaf@users.noreply.github.com>
2017-10-17 09:06:28 -0400

    Don't setup emacs and sway by default

--- install.sh
+++ install.sh
@@ -1,5 +1,35 @@
 #/bin/sh
 
+args=("$@")
+
+function flag_was_specified {
+    for arg in ${args[@]}; do
+        if [[ $arg == *$1* ]]; then
+            return 0
+        fi
+    done
+    false
+}
+
+function show_usage {
+    echo -e "\r\nInstall config files for zsh, tmux, nvim.\r\n"
+    echo -e "Additional flags:"
+    echo -e "--with-emacs    Also install emacs config"
+    echo -e "--with-sway     Also installs sway and i3bar config"
+}
+
+if flag_was_specified "help"; then
+    show_usage
+    exit
+fi
+
+for arg in "$@"
+do
+    if [[ $arg == *"help"* ]]; then
+        show_usage
+    fi
+done
+
 dotfiles_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$HOME"
@@ -9,14 +39,20 @@ ln -fs "$dotfiles_dir/tmux/tmux.conf" ".tmux.conf"
 cd "$HOME/.config"
 ln -fsn "$dotfiles_dir/nvim" "nvim"
 
-mkdir -p "$HOME/.config/sway"
-cd "$HOME/.config/sway"
-ln -fsn "$dotfiles_dir/sway/config" "config"
+for arg in "$@"
+do
+    if [[ $arg == *"with-emacs"* ]]; then
+        mkdir -p "$HOME/.emacs.d"
+        cd "$HOME/.emacs.d"
+        ln -fsn "$dotfiles_dir/emacs/init.el" "init.el"
+    fi
+    if [[ $arg == *"with-sway"* ]]; then
+        mkdir -p "$HOME/.config/sway"
+        cd "$HOME/.config/sway"
+        ln -fsn "$dotfiles_dir/sway/config" "config"
 
-mkdir -p "$HOME/.config/i3status"
-cd "$HOME/.config/i3status"
-ln -fsn "$dotfiles_dir/i3status/config" "config"
-
-mkdir -p "$HOME/.emacs.d"
-cd "$HOME/.emacs.d"
-ln -fsn "$dotfiles_dir/emacs/init.el" "init.el"
+        mkdir -p "$HOME/.config/i3status"
+        cd "$HOME/.config/i3status"
+        ln -fsn "$dotfiles_dir/i3status/config" "config"
+    fi
+done
