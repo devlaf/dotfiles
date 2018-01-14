ad6224958647189ed0e273e020a0387f6a6ef09c
devlaf <devlaf@users.noreply.github.com>
2018-01-14 18:14:11 -0500

    Add port knocking script

---
+++ zsh/scripts/knock.sh
@@ -0,0 +1,14 @@
+####################################################################
+# Port knocking script
+#   Usage: knock {host} {first_port_num} {second_port_num} etc.
+#
+####################################################################
+
+knock() {
+    HOST=$1
+    shift
+    for ARG in "$@"
+    do
+        nmap -Pn --host_timeout 100 --max-retries 0 -p $ARG $HOST
+    done
+}
