e09115915d45c89ad1953cde9b2cfbb9dc71a220
devlaf <devin@devlaf.io>
2018-05-27 12:14:33 -0400

    Update knock script

--- zsh/scripts/knock.sh
+++ zsh/scripts/knock.sh
@@ -9,6 +9,6 @@ knock() {
     shift
     for ARG in "$@"
     do
-        nmap -Pn --host_timeout 100 --max-retries 0 -p $ARG $HOST
+        nmap -Pn --host-timeout 100 --max-retries 0 -p $ARG $HOST
     done
 }
