af84c94d9398cb02288857d5e83f66f1e4075767
devlaf <devin@devlaf.io>
2020-08-25 16:35:38 -0500

    Add package check

--- install.sh
+++ install.sh
@@ -1,64 +1,77 @@
 #/bin/sh
-
+ 
 args=("$@")
-
+ 
 dotfiles_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+xdg_cfg=$([[ -z ${XDG_CONFIG_HOME+x} ]] && echo "$HOME/.config" || echo "$XDG_CONFIG_HOME")
+ 
+mkdir -p $xdg_cfg
+ 
 function flag_exists {
-    for arg in ${args[@]}; do
-        if [[ $arg == *$1* ]]; then
-            return 0
-        fi
-    done
-    false
+  for arg in ${args[@]}; do
+    if [[ $arg == *$1* ]]; then
+      return 0
+    fi
+  done
+  false
 }
-
+ 
 function show_usage {
-    echo -e "\r\nInstall config files for zsh, tmux, nvim.\r\n"
-    echo -e "Additional flags:"
-    echo -e "--with-emacs    Also install emacs config & quicklisp"
+  echo -e "\r\nInstall config files for zsh, tmux, nvim.\r\n"
+  echo -e "Additional flags:"
+  echo -e "--with-emacs      Also install emacs config"
+  echo -e "--with-quicklisp  Also install quicklisp stuff"
 }
-
+ 
 if flag_exists "help"; then
-    show_usage
-    exit
+  show_usage
+  exit
 fi
-
+ 
+# ----------------------------------------------
+# package check
+# ----------------------------------------------
+packages="zsh nvim tmux"
+if flag_exists "with-quicklisp"; then packages="${packages} emacs scbl";
+elif flag_exists "with-emacs"; then packages="${packages} emacs";
+fi
+ 
+for app in $packages
+do
+  if ! which $app >/dev/null 2>/dev/null; then 
+    echo "You need to install the package for $app" && exit 1
+  fi
+done
+ 
 # ----------------------------------------------
 # zsh
 # ----------------------------------------------
-cd "$HOME"
-ln -fs "$dotfiles_dir/zsh/zshrc" ".zshrc"
-
+ln -fsn "${dotfiles_dir}/zsh/zshrc" "${HOME}/.zshrc"
+ 
 # ----------------------------------------------
 # tmux
 # ----------------------------------------------
-cd "$HOME"
-ln -fs "$dotfiles_dir/tmux/tmux.conf" ".tmux.conf"
-
+ln -fsn "${dotfiles_dir}/tmux/tmux.conf" "${HOME}/.tmux.conf"
+ 
 # ----------------------------------------------
 # nvim
 # ----------------------------------------------
-mkdir -p "$HOME/.config"
-cd "$HOME/.config"
-ln -fsn "$dotfiles_dir/nvim" "nvim"
-
+ln -fsn "${dotfiles_dir}/nvim" "${xdg_cfg}/nvim"
+ 
 # ----------------------------------------------
 # emacs + quicklisp
 # ----------------------------------------------
-if flag_exists "with-emacs"; then
-    mkdir -p "$HOME/.emacs.d"
-    cd "$HOME/.emacs.d"
-    ln -fsn "$dotfiles_dir/emacs/init.el" "init.el"
-
+if flag_exists "with-emacs" || flag_exists "with-quicklisp"; then
+  mkdir -p "$HOME/.emacs.d"
+  ln -fsn "${dotfiles_dir}/emacs/init.el" "${HOME}/.emacs.d/init.el"
+fi
+ 
+if flag_exists "with-quicklisp"; then
     cd ~/.emacs.d/
     curl -O https://beta.quicklisp.org/quicklisp.lisp
     sbcl --load quicklisp.lisp --eval '(quicklisp-quickstart:install)' --eval '(quit)'
     mv ~/quicklisp ~/.emacs.d/quicklisp
-
+ 
     echo -e "(let ((quicklisp-init (merge-pathnames \".emacs.d/quicklisp/setup.lisp\" (user-homedir-pathname))))" >> ~/.sbclrc
     echo -e "(when (probe-file quicklisp-init) (load quicklisp-init)))" >> ~/.sbclrc
-
-    mkdir -p "$home/.emacs.d/themes"
-    ln -fsn "$dotfiles_dir/emacs/theme/nord-emacs/nord-theme.el" "$home/.emacs.d/themes"
 fi
