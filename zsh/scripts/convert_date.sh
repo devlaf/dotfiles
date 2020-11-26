a0ee6dc9ba9deb76cddc331d33e71dfc9fc1c90a
Devin LaFrance <devin@devlaf.io>
2020-11-25 18:12:42 -0600

    move zsh scripts to bin

--- zsh/scripts/convert_date.sh
+++
@@ -1,9 +0,0 @@
-epoch_convert() {
-  if [[ $# -eq 0 ]] ; then
-    echo 'Usage: epoch_convert {date_in_epoch_milli} --> outputs readable date'
-    return
-  fi
-
-  date -d @$(($1 / 1000))
-}
-
--- zsh/scripts/fetch_gravitar.sh
+++
@@ -1,9 +0,0 @@
-gravitar() {
-  if [[ $# -eq 0 ]] ; then
-    echo 'Usage: gravitar {email_address} --> fetches and saves to ./gravitar-{email_address}.png'
-    return
-  fi
-
-  curl "http://gravatar.com/avatar/$(echo -n $1 | md5sum)?s=300" > gravatar_$1.png
-}
-
