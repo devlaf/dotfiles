937ca3845930e25432ee098d1c93d20571ff46d8
Devin LaFrance <devin@devlaf.io>
2020-11-25 18:12:14 -0600

    add melodic and ditch a few oldies

--- zsh/ros/ros_setup.sh
+++ zsh/ros/ros_setup.sh
@@ -1,38 +1,29 @@
+####################################################
+# ROS shell setup
+#  (and why humanity will never escape python 2)
 #
-# ROS bash setup 
-# (note: Python 2.7 required for some older packages)
-#
+####################################################
+
+melodic() {
+  setup="/opt/ros/melodic/setup.zsh"
+  if [ ! -d "$setup" ]; then
+      echo "shell config not found at $setup"
+      return 1
+  fi
+  source $setup
+}
 
 lunar() {
-  source /opt/ros/lunar/setup.zsh
+  setup="/opt/ros/lunar/setup.zsh"
+  if [ ! -d "$setup" ]; then                                                                             
+      echo "shell config not found at $setup"                                                            
+      return 1                                                                                           
+  fi
+  source $setup
+
   export PYTHONPATH=/opt/ros/lunar/lib/python2.7/site-packages:$PYTHONPATH
   export PKG_CONFIG_PATH="/opt/ros/lunar/lib/pkgconfig:$PKG_CONFIG_PATH"
 
   alias catkin_make="catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python2 -DPYTHON_INCLUDE_DIR=/usr/include/python2.7 -DPYTHON_LIBRARY=/usr/lib/libpython2.7.so"
 }
 
-jade() {
-  source /opt/ros/jade/setup.zsh
-  export PYTHONPATH=/opt/ros/jade/lib/python2.7/site-packages:$PYTHONPATH
-  export PKG_CONFIG_PATH="/opt/ros/jade/lib/pkgconfig:$PKG_CONFIG_PATH"
-
-  # If you wind up with a standard package path:
-  # export ROS_PACKAGE_PATH=/path/to/your/package/path:$ROS_PACKAGE_PATH
-
-  alias catkin_make="catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python2 -DPYTHON_INCLUDE_DIR=/usr/include/python2.7 -DPYTHON_LIBRARY=/usr/lib/libpython2.7.so"
-
-  source /usr/share/gazebo/setup.sh
-}
-
-indigo() {
-  source /opt/ros/indigo/setup.zsh
-  export PYTHONPATH=/usr/lib/python2.7/site-packages:/opt/ros/indigo/lib/python2.7/site-packages:$PYTHONPATH
-  export PKG_CONFIG_PATH="/opt/ros/indigo/lib/pkgconfig:$PKG_CONFIG_PATH"
-
-  # If you wind up with a standard package path:
-  # export ROS_PACKAGE_PATH=/path/to/your/package/path:$ROS_PACKAGE_PATH
-
-  alias catkin_make="catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python2 -DPYTHON_INCLUDE_DIR=/usr/include/python2.7 -DPYTHON_LIBRARY=/usr/lib/libpython2.7.so"
-
-  source /usr/share/gazebo/setup.sh
-}
